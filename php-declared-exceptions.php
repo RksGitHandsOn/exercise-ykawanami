ccfef335f3c479688bd831a84ac5e946d634d50f
Yuu Kawanami <y.kawanami@rakus.co.jp>
2015-12-07 17:56:01 +0900

    名前の昇順でソートする #1

--- php-declared-exceptions.php
+++ php-declared-exceptions.php
@@ -4,4 +4,6 @@ $declaredExceptions = array_filter(get_declared_classes(), function($className) 
   return strrpos($className, 'Exception') !== false;
 });
 
+sort($declaredExceptions);
+
 var_dump($declaredExceptions);
